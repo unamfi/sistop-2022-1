e56b819d2e79ff20625c43ebbb0cec88ecb2fd98
Ricardo Juarez <kronos_20_11@hotmail.com>
2021-09-28 21:10:50 -0500

    terminamos la ejecucion con un "return 0"

---
+++ practicas/3/prueba.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Vehiculo{
+    int ruedas;
+    int precio;
+    char color[15];
+    char nombre[15];
+};
+
+void presentacion(struct Vehiculo v){
+
+    printf("este vehiculo se llama %s \n tiene %i ruedas\n es de color %s\n cuesta $%i\n",v.nombre, &v.ruedas, v.color, &v.precio);
+}
+
+int main(){
+
+    struct Vehiculo vehiculos[] = { {2,100, "verde", "moto"}, {4,100, "rojo", "auto"}, {6,1000, "negro", "camion"}};
+    int count = 3;
+
+    for (size_t i = 0; i < count; i++){
+        presentacion(vehiculos[i]);
+    }
+    
+}
