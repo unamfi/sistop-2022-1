fba7de625e526bb013aaecdd10ce552f6d5b81c8
albertogfm <albertogarciafigueroam@gmail.com>
2021-10-25 01:21:36 -0500

    Creación tienda y primer modulo creado

--- proyectos/1/GarciaFigueroaAlberto-GarciaEdgar/CÓDIGO/Tienda.py
+++ proyectos/1/GarciaFigueroaAlberto-GarciaEdgar/CÓDIGO/Tienda.py
@@ -1,2 +1,21 @@
+import threading
+import sys
+import time
+import random
+
 class Tienda:
-    aforo=10
+    #Declaramos los productos que se venden en la tienda
+    productos = {1:["disfraz de Dracula","Disfraz de zombie","disfraz de hombre lobo","disfraz de momia"], 2:["chocolate","caramelo","milkyway","pikafresas"]}
+    global aforo
+    def __init__(self,var):
+        var = 0
+    
+    #Dejamos pasar al cliente en cola
+    def pass_client(self, cliente):
+        if aforo<11
+            print("Bienvenido a la tienda cliente numero"+self.visita+" a la tienda")
+            aforo =  aforo + 1
+    
+
+
+    
