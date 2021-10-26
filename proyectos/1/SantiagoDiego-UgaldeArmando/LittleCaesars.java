6beeeaa0c51eb6b34aa6af6ac1dadd5556c6529b
Armando Ugalde <armauv@gmail.com>
2021-10-26 11:13:32 -0500

    Inicia Mostrador

--- proyectos/1/SantiagoDiego-UgaldeArmando/LittleCaesars.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/LittleCaesars.java
@@ -2,9 +2,11 @@ public class LittleCaesars {
     public static void main(String[] args) {
         // TODO: Añadir variable de condición para detener operaciones.
         // TODO: Añadir impresión de acciones.
+        System.out.println("¡Little Caesars inicia sus operaciones!");
         Fila fila = new Fila();
         Horno horno = new Horno();
         Calle.iniciar(fila);
         Cocina.iniciar(horno);
+        Mostrador.iniciar(fila, horno);
     }
 }
