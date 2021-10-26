46fdee4833902b9d2391612eec7498055c662f43
Armando Ugalde <armauv@gmail.com>
2021-10-26 14:57:31 -0500

    Agrega impresión de mensajes en Pizzero

--- proyectos/1/SantiagoDiego-UgaldeArmando/Pizzero.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Pizzero.java
@@ -1,32 +1,45 @@
+import utilidades.Impresor;
+
 import java.util.concurrent.TimeUnit;
 
 public class Pizzero implements Runnable {
     private final Cocina cocina;
+    private final int id;
     private final int TIEMPO_DE_PREPARACION = 5;
 
-    public Pizzero(Cocina cocina) {
+    public Pizzero(Cocina cocina, int id) {
         this.cocina = cocina;
+        this.id = id;
     }
 
-    private Pizza hacerPizza() {
-        int numeroDePizza = cocina.obtenerNumeroDePizza();
+    private static void imprimirMensaje(int id, String mensaje) {
+        Impresor.imprimirBlanco("Pizzero " + id, mensaje);
+    }
 
+    private Pizza prepararPizza() {
+        int numeroDePizza = cocina.obtenerNumeroDePizza();
+        Pizza pizza = new Pizza(numeroDePizza);
+
+        imprimirMensaje(id, "Haciendo " + pizza);
         try {
             TimeUnit.SECONDS.sleep(TIEMPO_DE_PREPARACION);
         } catch (InterruptedException e) {
             throw new RuntimeException("PIZZERO FUE INTERRUMPIDO");
         }
+        imprimirMensaje(id, "La " + pizza + " está lista para hornear");
 
-        return new Pizza(numeroDePizza);
+        return pizza;
     }
 
     @Override
     public void run() {
+        imprimirMensaje(id, "Empezará a hacer pizzas");
         while (true) {
-            Pizza pizzaCruda = this.hacerPizza();
+            Pizza pizzaCruda = this.prepararPizza();
             cocina.ponerEnHorno(pizzaCruda);
         }
     }
 }
 
 // TODO: Modificar constantes de tiempo públicas
+// TODO: Refactorizar nombres de métodos
