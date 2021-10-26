67dd89488821e8fde07577b1c1b824afdaec4be6
Iñaky <inaky.ordiales@gmail.com>
2021-10-26 15:40:42 -0500

    Se agregó el formato de orden para empezar a trabajar con los mecanismos de sincronización

--- proyectos/1/OrdialesI�aky/proyecto1.c
+++ proyectos/1/OrdialesI�aky/proyecto1.c
@@ -1,12 +1,69 @@
-/* Proyecto 1 de la materia de Sistemas Operativos */
+/*
+  Proyecto 1 para la materia de Sistemas Operativos, grupo 6.
+
+  Desarrollado por IÑAKY ORDIALES CABALLERO
+
+  Profesor: Gunnar Eyal Wolf Iszaevich
+  Semestre: 2022-1
+  Facultad de Ingeniería, UNAM.
+
+  Instrucciones.
+  Implementen un programa que simule un proceso de la vida real
+  que presente características de concurrencia, necesariamente
+  empleando hilos o procesos simultáneos. 
+  Implementen una interfaz de ususario profesional, que demuestre
+  cómo la sincronización puede ser bella y elegante.
+
+*/
+
+/*
+         "Simulación de centro comercial en pandemia"
+
+  Para este desarrollo se decicdió el simular la afluencia y 
+  compras de un centro comercial durante esta época de pandemia. 
+  El usuario indicará el semáforo que limita el cupo máximo de
+  las instalaciones, el número de clientes que desean acceder al
+  centro comercial y el tiempo en segundos que permanecerán 
+  abiertas las instalaciones.
+
+
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <ncurses.h>
 
+// Declaración funciones prototipo
+
+
+// Declaración semáforos (torniquetes y mutex) globales
+
+
+// Declaración e inicialización de variables de condición
+
+
+// Declaración variables globales
+
+
+// Hilo cliente
+
+// Hilo cajero
+
+// Hilo seguridad
+
+
+
+
+
 int main() {
+  
+  /*
 
   // inititalizes the screen
   // sets up memory and clears the screen
@@ -39,7 +96,7 @@ int main() {
 
   // deallocates memory and ends ncurses
   endwin();
-
+  */
 
   return 0;
 }
