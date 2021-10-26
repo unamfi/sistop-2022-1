b4261d8bc50ce2910b8c74e2492a95124d8286af
Armando Ugalde <armauv@gmail.com>
2021-10-25 19:19:20 -0500

    Implementa fila de clientes

--- proyectos/1/SantiagoDiego-UgaldeArmando/Fila.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Fila.java
@@ -1,2 +1,15 @@
 public class Fila {
+    private final ColaSegura<Cliente> cola;
+
+    public Fila() {
+        this.cola = new ColaSegura<>(Integer.MAX_VALUE);
+    }
+
+    private void formarCliente(Cliente cliente) {
+        this.cola.encolar(cliente);
+    }
+
+    private Cliente llamarAlPrimero() {
+        return this.cola.desencolar();
+    }
 }
