89f50399573ee859b3fcd57a741cc0e8143b4156
Armando Ugalde <armauv@gmail.com>
2021-10-25 21:43:48 -0500

    Implementa funcionalidad de Horno

--- proyectos/1/SantiagoDiego-UgaldeArmando/Horno.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Horno.java
@@ -1,2 +1,25 @@
+import java.util.concurrent.TimeUnit;
+
 public class Horno {
+    public static int MAX_PIZZAS = 15;
+    public static int TIEMPO_DE_HORNEADO = 10;
+    private final ColaSegura<Pizza> pizzasListas;
+
+    public Horno() {
+        this.pizzasListas = new ColaSegura<>(MAX_PIZZAS);
+    }
+
+    public void hornearPizza(Pizza pizza) {
+        try {
+            TimeUnit.SECONDS.sleep(TIEMPO_DE_HORNEADO);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("HORNO FUE INTERRUMPIDO");
+        }
+        pizza.marcarComoHorneada();
+        this.pizzasListas.encolar(pizza);
+    }
+
+    public Pizza sacarPizza() {
+        return this.pizzasListas.desencolar();
+    }
 }
