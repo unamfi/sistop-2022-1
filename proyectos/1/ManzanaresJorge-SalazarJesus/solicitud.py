ff1b0ced42ef50a31578bf72bc7c708d2a645b10
Jesus <jesus@pc-jsd-virtual.fi.unam>
2021-10-26 17:29:13 -0500

    Versión funcional del programa con hilos implementados

--- proyectos/1/ManzanaresJorge-SalazarJesus/solicitud.py
+++ proyectos/1/ManzanaresJorge-SalazarJesus/solicitud.py
@@ -72,3 +72,35 @@ def Visitante(id):
     profesor_libre.release()
 
 
+#Inicio del programa
+while(error == 1):
+    #Recopilación de datos a partir del usuario
+    try:
+        num_jovenes = int(input('Ingrese el número de estudiantes jóvenes en el grupo: '))
+        num_mayores = int(input('Ingrese el número de estudiantes mayores en el grupo: '))
+        num_visitantes = int(input('Ingrese el número de visitantes extra para el profesor: ')) + 1
+        severidad_profesor = int(input('Ingrese que tan estricto es el profesor (0-50): '))
+        num_estudiantes = num_jovenes + num_mayores
+        error = 0
+    except ValueError as e1:
+        print("Tipo de dato equivocado")
+
+
+#Inicialización de los hilos
+#Hilo del jefe de grupo
+threading.Thread(target=JefeGrupo,args=[-1]).start()
+
+#Hilo del profesor
+threading.Thread(target=Profesor, args=[]).start()
+
+#Hilos de estudiantes jóvenes
+for k in range(num_jovenes):
+    threading.Thread(target=Estudiante,args=[k, "joven"]).start()
+
+#Hilos de estudiantes mayores
+for i in range (num_mayores):
+    threading.Thread(target=Estudiante,args=[num_jovenes + i, "mayor"]).start()
+
+#Hilos de visitantes
+for j in range (num_visitantes - 1):
+    threading.Thread(target=Visitante,args=[j]).start()
