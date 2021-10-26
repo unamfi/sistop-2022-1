74096f73ac05148cd06133d7e2ce77c2196ef7b8
Armando Ugalde <armauv@gmail.com>
2021-10-25 19:58:28 -0500

    Implementa Cliente

--- proyectos/1/SantiagoDiego-UgaldeArmando/Cliente.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Cliente.java
@@ -1,2 +1,16 @@
 public class Cliente {
+    public static int MAX_PIZZAS = 10;
+    private int pizzasDeseadas;
+
+    public Cliente() {
+        this.pizzasDeseadas = Utilidades.obtenerEnteroPositivoAleatorio(10);
+    }
+
+    public int obtenerPizzasDeseadas() {
+        return pizzasDeseadas;
+    }
+
+    public void actualizarPizzasDeseadas(int nuevoValor) {
+        this.pizzasDeseadas = nuevoValor;
+    }
 }
