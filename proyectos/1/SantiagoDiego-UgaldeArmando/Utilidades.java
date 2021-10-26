317bedbeceb9e5628f9c7201a967617a940cbc2c
Armando Ugalde <armauv@gmail.com>
2021-10-25 19:56:29 -0500

    Añade clase de utilidades

---
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Utilidades.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Utilidades {
+    public static int obtenerEnteroPositivoAleatorio(int max) {
+        Random ran = new Random();
+        return ran.nextInt(max) + 1;
+    }
+}
