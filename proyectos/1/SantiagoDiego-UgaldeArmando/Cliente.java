bc2a44be29cbf4bd6593cf45f8f21c010c91a6a6
Armando Ugalde <armauv@gmail.com>
2021-10-26 13:28:18 -0500

    Agrega funcionalidad de IDs a Cliente

--- proyectos/1/SantiagoDiego-UgaldeArmando/Cliente.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/Cliente.java
@@ -1,10 +1,14 @@
+import utilidades.AcumuladorSeguro;
 import utilidades.UtilidadesNumericas;
 
 public class Cliente {
+    private static final AcumuladorSeguro clientesGenerados = new AcumuladorSeguro();
     public static int MAX_PIZZAS = 10;
+    private final int id;
     private int pizzasDeseadas;
 
     public Cliente() {
+        this.id = clientesGenerados.incrementarYObtener();
         this.pizzasDeseadas = UtilidadesNumericas.obtenerEnteroPositivoAleatorio(MAX_PIZZAS);
     }
 
@@ -14,5 +18,10 @@ public class Cliente {
 
     public void actualizarPizzasDeseadas(int nuevoValor) {
         this.pizzasDeseadas = nuevoValor;
+    }
+
+    @Override
+    public String toString() {
+        return "cliente " + id;
     }
 }
