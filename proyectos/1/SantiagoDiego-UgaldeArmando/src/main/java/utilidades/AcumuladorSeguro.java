a8cc5f51d6cf02fa4364bd945db232e0f46cf11b
Armando Ugalde <armauv@gmail.com>
2021-10-26 20:05:34 -0500

    Agrega documentación en AcumuladorSeguro

--- proyectos/1/SantiagoDiego-UgaldeArmando/src/main/java/utilidades/AcumuladorSeguro.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/src/main/java/utilidades/AcumuladorSeguro.java
@@ -6,6 +6,13 @@ package utilidades;
 public class AcumuladorSeguro {
     private int valor = 0;
 
+    /**
+     * Se utiliza el patrón de diseño de monitor soportado por Java donde, al llamarse este método, se adquiere el Lock
+     * de este objeto, y al terminar su ejecución se libera. Lo anterior hace posible que la operación se lleve a cabo
+     * de forma atómica.
+     *
+     * @return Nuevo valor
+     */
     public synchronized int incrementarYObtener() {
         return ++valor;
     }
