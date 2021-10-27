5ab3d849fe021400ab5b95a40c1a3849ddc1ce40
Armando Ugalde <armauv@gmail.com>
2021-10-26 20:02:58 -0500

    Agrega comentarios en LittleCaesars

--- proyectos/1/SantiagoDiego-UgaldeArmando/src/main/java/LittleCaesars.java
+++ proyectos/1/SantiagoDiego-UgaldeArmando/src/main/java/LittleCaesars.java
@@ -3,10 +3,16 @@ import utilidades.Constantes;
 public class LittleCaesars {
     public static void main(String[] args) {
         // TODO: Añadir variable de condición para detener operaciones.
+
+        // Inicializar constantes importantes
         Constantes.obtenerDeJson();
         System.out.println("¡Little Caesars inicia sus operaciones!");
+
+        // Fila y horno serán recursos que se comparten por los productores y consumidores
         Fila fila = new Fila();
         Horno horno = new Horno();
+
+        // Iniciar productores y consumidores proporcionando las dependencias necesarias
         Calle.iniciar(fila);
         Cocina.iniciar(horno);
         Mostrador.iniciar(fila, horno);
