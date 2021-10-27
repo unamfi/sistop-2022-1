5b5c8606dcfc0f6facce6790091159a2f00ec226
edlayton1 <edlayton0@gmail.com>
2021-10-27 00:33:24 -0500

    Programa Funcional :sparkles:

--- proyectos/1/MorenoEduardo/aeropuerto.py
+++ proyectos/1/MorenoEduardo/aeropuerto.py
@@ -8,7 +8,7 @@ import threading
 import time
 import random
 import sys
-
+import os 
 #Variables:
 
 num_maletas = 0
@@ -21,9 +21,9 @@ num_serf = 0
 maletas_ = threading.Semaphore(0)  #💼
 pasajeros_ = threading.Semaphore(0) #👤
 
-mutex_ingreso = threading.Semaphore(1)
-mutex_maletas = threading.Semaphore(1)
-mutex_pasajeros = threading.Semaphore(1)
+mutex_ingreso = threading.Semaphore(3)
+mutex_maletas = threading.Semaphore(2)
+mutex_pasajeros = threading.Semaphore(2)
 mutex_recoger = threading.Semaphore(2)
 
 
@@ -31,50 +31,84 @@ def maletas():
     global num_maletas
     global mutex_maletas
     global limite_pasajeros
+    global limite_maletas
+    global maletas_
 
     mutex_maletas.acquire()
 
-    if (maletas == 15):
+    if (num_maletas == 15 and limite_maletas==False):
         limite_maletas = True
         mutex_maletas.release()
-    elif (num_maletas >= 0 and num_maletas < 15 ):
+    elif (num_maletas >= 7 and num_maletas < 15 ):
         num_maletas += 1
-        mutex_maletas.release()
+        print("Ha llegado ", end="")
         ingresa("maleta")
-        maletas_.acquire()
+        print("")
         
-    else:
-        print("Algo anda mal (maletas)")
+        #ingresa("maleta")
+        #pantalla()
+        
         mutex_maletas.release()
+        #
+    else:
+        if(limite_maletas == False):
+            num_maletas += 1
+            print("Ha llegado ", end="")
+            ingresa("maleta")
+            print("")
+            mutex_maletas.release()
+            maletas_.acquire()
+        elif(limite_maletas == False and num_maletas != 15):
+            limite_maletas = True
+        else:
+            mutex_maletas.release()
+            print("¡No hay espacio en la cinta!")
 
-
-def pasajeros():
+def pasajeros(recoge):
     global num_pasajeros
     global limite_maletas
+    global limite_pasajeros
     global mutex_pasajeros
+    global pasajeros_
+    global maletas_
 
     mutex_pasajeros.acquire()
 
-    if (maletas == 20):
+    if (num_pasajeros == 20 and limite_pasajeros == False):
         limite_pasajeros = True
         mutex_pasajeros.release()
-    elif (num_pasajeros >= 0 and num_pasajeros < 20 ):
-        num_pasajeros += 1
+    elif (num_pasajeros >= 7 and num_pasajeros <= 20 and recoge == 1):
+           
+        #ingresa("pasajero")
+        #pantalla()
+        pasajeros_.release()
+        maletas_.release()
         mutex_pasajeros.release()
-        ingresa("pasajero")
-        pasajeros_.acquire()
+        recoger()
         
     else:
-        print("Algo anda mal (pasajeros)")
-        mutex_pasajeros.release()
+        if(limite_pasajeros == False):
+            num_pasajeros += 1
+            print("Ha llegado ", end="")
+            ingresa("pasajero")
+            print("")
+            mutex_pasajeros.release()
+            pasajeros_.acquire()
+        elif(limite_pasajeros == False and num_maletas !=20):
+            limite_pasajeros == True
+        else:
+            mutex_pasajeros.release()
+            print("¡No hay espacio alrededor de la cinta!")
+        
+    
 
 def ingresa(tipo):
-    mutex_ingreso.acquire()
+    
     if(tipo == "maleta"):
-        print("💼")
+        print("💼", end="")
     elif(tipo == "pasajero"):
-        print("👤")
-    mutex_ingreso.release()
+        print("👤", end="")
+    
 
 def borrarPantalla(): #Borra pantalla dependiendo del sistema operativo
     if os.name == "posix":
@@ -83,38 +117,74 @@ def borrarPantalla(): #Borra pantalla dependiendo del sistema operativo
         os.system ("cls")
 
 def imprimirProblema():
-    print("======== La cinta del aeropuerto ========")
+    print("                         ======== La cinta del aeropuerto ========               ")
     print("Nos encontramos en un aeropuerto, en el aeropuerto tenemos una cinta que trae el equipaje,"
     + " alrededor de ella hay gente esperando para recogerlo, pasa el tiempo, entra y sale equipaje, así como personas, no todo" +
     " el equipaje saldrá, pero observemos como se mueve el ritmo de maletas en el aeropuerto")
 
-def recoger(desarrollador,icon):
+def recoger():
     global num_maletas
     global num_pasajeros
     global limite_maletas
     global limite_pasajeros
     mutex_recoger.acquire()
 
+    num_maletas = num_maletas -1
+    num_pasajeros = num_pasajeros -1
     if (num_pasajeros == 0 or num_maletas == 0):
         print("No hay nada")
-    
+    elif(num_pasajeros >= 1 and num_maletas >= 1):
+        print("Se va ", end="")
+        ingresa("pasajero")
+        print("")
+        print("Se va ", end="")
+        ingresa("maleta")
+        print("")
+    else:
+        print("Todo mal")
     mutex_recoger.release()
 
-
+def pantalla():
+    global num_pasajeros
+    global num_maletas
+    a = 0
+    b = 0
+    mutex_ingreso.acquire()
+    while a < num_maletas: 
+        ingresa("maleta")
+        a += 1
+    print(" ")
+    while b < num_pasajeros:
+        ingresa("pasajero")
+        b += 1 
+    
+    
+    mutex_ingreso.release()
 
 
 try:
     while True:
+        #
+        #borrarPantalla()
+        #
+        #imprimirProblema()
+        #
         if round(random.choice([0,1])) == 0:
             threading.Thread(target = maletas, args = []).start()
-            time.sleep(0.5)
+            time.sleep(2)
+            pantalla()
             print(threading.active_count())
         else:
-            threading.Thread(target = pasajeros, args = []).start()
-            time.sleep(0.5)
+            threading.Thread(target = pasajeros, args = [round(random.choice([0,1]))]).start()
+            time.sleep(2)
+            pantalla()
+            #time.sleep(2)
             print(threading.active_count())
 
-
+        #
 except KeyboardInterrupt:
-    print("║║ Vámonos a casa ║║")
+    print("             ║║ Vámonos a casa ║║               ")
+    
+    sys.exit()
+except AttributeError:
     sys.exit()
