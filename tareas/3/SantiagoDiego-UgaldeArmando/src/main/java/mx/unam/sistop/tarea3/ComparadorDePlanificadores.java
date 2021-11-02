f54e5cfc4468a209042fb56f1465c4ec35064140
Armando Ugalde <armauv@gmail.com>
2021-11-02 15:44:48 -0600

    Implementa ejecución de rondas de simulación

--- tareas/3/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea3/ComparadorDePlanificadores.java
+++ tareas/3/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea3/ComparadorDePlanificadores.java
@@ -1,6 +1,13 @@
 package mx.unam.sistop.tarea3;
 
 public class ComparadorDePlanificadores {
+    private static final int RONDAS = 5;
+
     public static void main(String[] args) {
+        for (int i = 0; i < RONDAS; i++) {
+            System.out.println("Ronda " + (i + 1) + ":");
+            Planificadores.ejecutar(CargaAleatoria.generar());
+            System.out.println();
+        }
     }
 }
