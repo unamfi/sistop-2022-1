823477fed57adfd2703678063453d45a65657c67
Armando Ugalde <armauv@gmail.com>
2021-11-02 15:44:24 -0600

    Agrega clase Planificadores

---
+++ tareas/3/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea3/Planificadores.java
@@ -0,0 +1,8 @@
+package mx.unam.sistop.tarea3;
+
+public class Planificadores {
+
+    public static void ejecutar(CargaAleatoria cargaAleatoria) {
+        System.out.println(cargaAleatoria);
+    }
+}
