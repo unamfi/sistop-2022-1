5964dcd8ed6ca023d790a3c6d2b394fb4521462d
Armando Ugalde <armauv@gmail.com>
2021-11-02 15:15:23 -0600

    Implementa generación de CargaAleatoria

--- tareas/3/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea3/CargaAleatoria.java
+++ tareas/3/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea3/CargaAleatoria.java
@@ -1,12 +1,36 @@
 package mx.unam.sistop.tarea3;
 
 import lombok.Value;
+import mx.unam.sistop.tarea3.utilidades.UtilidadesNumericas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Value
 public class CargaAleatoria {
+    private static final int MAX_TIEMPO_DE_EJECUCION_POR_PROCESO = 10;
+    private static final int TOTAL_PROCESOS = 10;
+
     List<Proceso> procesos;
+
+    public static CargaAleatoria generar() {
+        List<Proceso> procesosGenerados = new ArrayList<>();
+
+        int tiempoTotal = 0, tiempoDeLlegadaAnterior = 0;
+        for (int i = 0; i < TOTAL_PROCESOS; i++) {
+            int tiempoDeLlegada = UtilidadesNumericas.obtenerEnteroInclusivo(tiempoDeLlegadaAnterior, tiempoTotal - 1);
+            tiempoDeLlegadaAnterior = tiempoDeLlegada;
+
+            int tiempoDeEjecucion = UtilidadesNumericas.obtenerEnteroPositivoAleatorio(MAX_TIEMPO_DE_EJECUCION_POR_PROCESO);
+            tiempoTotal += tiempoDeEjecucion;
+
+            Proceso proceso = new Proceso(tiempoDeLlegada, tiempoDeEjecucion);
+
+            procesosGenerados.add(proceso);
+        }
+
+        return new CargaAleatoria(procesosGenerados);
+    }
 
     public int getTiempoTotalDeEjecucion() {
         return procesos.stream()
