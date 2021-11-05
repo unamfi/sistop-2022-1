03ea04ac6481968406c731348d9f746ca388a6e3
Iñaky <inaky.ordiales@gmail.com>
2021-11-04 23:35:02 -0600

    Codigo final, pero sin lograr depurar todo

--- tareas/3/OrdialesInaky/tarea3.cpp
+++ tareas/3/OrdialesInaky/tarea3.cpp
@@ -1,3 +1,17 @@
+/*     TAREA 3 de la asignatura de Sistemas Operativos
+
+       " Algoritmos de administración de procesos "
+
+       Elaborado por: Ordiales Caballero, Iñaky
+       Noviembre del 2021.
+       
+
+       El código tiene todos los algoritmos y funcionan adecuadamente, pero en ocasiones
+       marca un segmentation fault en el FB que no pude desbuguear, por lo que no 
+       se ejecutara este algoritmo para evitar problemas. 
+       Al ejecutar el código te muestra la información en tablas conforme se va simulando.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -22,6 +36,8 @@ struct Proceso{
 };
 
 
+// Estrctura para comparar ordenar por el proceso más corto
+
 struct CompareShort{
   bool operator()(Proceso* const& p1, Proceso* const& p2){
     return ((p1->requerido) > (p2->requerido));
@@ -41,11 +57,15 @@ void imprimirResultados(struct Proceso*, int);
 void simulacion(struct Proceso*, int);
 
 
+
 // Algoritmos de administración de procesos                                     
 
+// First in first out, la idea es tener una cola y se forman
+// para ser procesados como van llegando, de inicio hasta
+// que acaben.
 void fifo(struct Proceso *procesos, int num){
 
-  struct Proceso *actual;
+  struct Proceso *actual=NULL;
   queue<struct Proceso*> cola;
   int tiempo = 0;
   int contadorLlegada = 0;
@@ -75,14 +95,14 @@ void fifo(struct Proceso *procesos, int num){
       }
     }
 
-    // imprime nombre de trabajo
+    // imprime proceso en ejecucion
     if(ocupado){
       printf("%c", actual->nombre);
       actual->ejecutado++;
     }
     tiempo++;
 
-    // Si ya llegó el proceso -> se agrega a la fila.
+    // Si ya llegó un proceso -> se agrega a la fila.
     if(contadorLlegada<num && procesos[contadorLlegada].llegada <= tiempo){
       procesos[contadorLlegada].ejecutado = 0;
       cola.push(&procesos[contadorLlegada]);
@@ -90,7 +110,7 @@ void fifo(struct Proceso *procesos, int num){
     }
     
     // Si ya acabó se calculan resultados individuales
-    if(actual->ejecutado == actual->requerido){
+    if(actual!=NULL && actual->ejecutado == actual->requerido){
       actual->termina = tiempo;
       actual->tiempo = actual->termina - actual->llegada;
       actual->espera = actual->tiempo - actual->requerido;
@@ -110,9 +130,13 @@ void fifo(struct Proceso *procesos, int num){
 
 }
 
+
+// La idea es que todos los procesos de la fila tienen un tiempo
+// predeterminado de ejecución y después se tienen que volver a
+// formar al final. Así hasta acabar.
 void roundRobin(struct Proceso *procesos, int num, int q){
 
-  struct Proceso *actual;
+  struct Proceso *actual=NULL;
   queue<struct Proceso*> cola;
   int tiempo = 0;
   int contadorLlegada = 0;
@@ -121,7 +145,7 @@ void roundRobin(struct Proceso *procesos, int num, int q){
   // ocupado -> procesador está libre
   int ocupado = 0;
   int contadorQ = 1;
-  struct Proceso *aux;
+  
 
   printf("\n\n\n RR con q=%d:  ", q);
 
@@ -160,7 +184,7 @@ void roundRobin(struct Proceso *procesos, int num, int q){
     }
     
     // Si ya acabó se calculan resultados individuales
-    if(actual->ejecutado == actual->requerido){
+    if(actual!=NULL && actual->ejecutado == actual->requerido){
       ocupado = 0;
       actual->termina = tiempo;
       actual->tiempo = actual->termina - actual->llegada;
@@ -173,9 +197,11 @@ void roundRobin(struct Proceso *procesos, int num, int q){
       if(contadorLlegada == num && cola.size()==0){
 	fin=1;
       }
-    }else if(contadorQ==q){
+    }else if(contadorQ==q){ // si se le acabo el tiempo, se forma
       ocupado = 0;
-      cola.push(actual);
+      if(actual!=NULL){
+	cola.push(actual);
+      }
       contadorQ = 0;
     }
     
@@ -187,9 +213,14 @@ void roundRobin(struct Proceso *procesos, int num, int q){
 }
 
 
+// Aquí se trata de que se ejecute el proceso más corto que esté formado
+// en la fila. Una vez termine se vuelve a checar la fila por el proceso
+// más corto. Para su implementación usé una priority queue que se acomoda
+// de tiempo requerido menor a mayor. Y cada que llega un proceso éste
+// se inserta según le corresponde en la fila.
 void shortNext(struct Proceso *procesos, int num){
 
-  struct Proceso *actual;
+  struct Proceso *actual=NULL;
   priority_queue<Proceso*, vector<Proceso*>, CompareShort> cola;
   int tiempo = 0;
   int contadorLlegada = 0;
@@ -234,7 +265,7 @@ void shortNext(struct Proceso *procesos, int num){
     }
     
     // Si ya acabó se calculan resultados individuales
-    if(actual->ejecutado == actual->requerido){
+    if(actual!=NULL && actual->ejecutado == actual->requerido){
       actual->termina = tiempo;
       actual->tiempo = actual->termina - actual->llegada;
       actual->espera = actual->tiempo - actual->requerido;
@@ -255,9 +286,16 @@ void shortNext(struct Proceso *procesos, int num){
 }
 
 
+// La lógica de fb me parece que está bien onstruída y en ocasiones corre
+// sin problemas, pero en otras marca un segmentation fault y aborta
+// la ejecución. Traté de entender por qué pasaba esto, pero no pude.
+// Su idea es que hay varias colas y después de determinados quantums
+// de ejecución, un proceso es degradado a una cola de menor prioridad.
+// Para esto utilize un arreglo de colas y un contador de quantums, si
+// se le terminaban los quantums y le faltaba ejecución, se degradaba.
 void fb(struct Proceso *procesos, int num, int n, int q){
   
-  struct Proceso *actual;
+  struct Proceso *actual=NULL;
   queue<struct Proceso*> colas[6];
   int tiempo = 0;
   int contadorLlegada = 0;
@@ -266,7 +304,6 @@ void fb(struct Proceso *procesos, int num, int n, int q){
   // ocupado -> procesador está libre
   int ocupado = 0;
   int contadorQ = 1;
-  struct Proceso *aux;
   int valorQ;
   int numCola = 0;
 
@@ -321,7 +358,7 @@ void fb(struct Proceso *procesos, int num, int n, int q){
     }
     
     // Si ya acabó se calculan resultados individuales
-    if(actual->ejecutado == actual->requerido){
+    if(actual!=NULL && actual->ejecutado == actual->requerido){
       ocupado = 0;
       actual->termina = tiempo;
       actual->tiempo = actual->termina - actual->llegada;
@@ -341,7 +378,11 @@ void fb(struct Proceso *procesos, int num, int n, int q){
       }
     }else if(contadorQ==valorQ){
       ocupado = 0;
-      colas[numCola+1].push(actual);
+      if(numCola<5){
+	colas[numCola+1].push(actual);
+      }else{
+	colas[numCola].push(actual);
+      }
       contadorQ = 0;
     }
     
@@ -352,10 +393,18 @@ void fb(struct Proceso *procesos, int num, int n, int q){
 
 }
 
+
+// Selfish round robin trata de que se tienen dos colas, una de procesos
+// nuevos y una de procesos "aceptados". Cada una tiene una proporción
+// de crecimiento de su prioridad, entonces hasta que los nuevos no lleguen
+// a la prioridad de los aceptados no podrán entrar a ejecución.
+// Se favorece al que se está ejecutando para no perder el control hasta que
+// un nuevo lo iguale o rebase en prioridad. Los aceptados no lo pueden
+// quitar porque su prioridad crece al mismo ritmo.
+// Para la implementación use colas con llaves de prioridad para el proceso.
 void selfRR(struct Proceso *procesos, int num, int a, int b){
-  
-  struct Proceso *actual;
-  int prioridad;
+
+  struct Proceso *actual=NULL, *aux=NULL;
   queue<pair<int, struct Proceso*>> colaNuevos, colaAceptados;
   int tiempo = 0;
   int contadorLlegada = 0;
@@ -363,48 +412,42 @@ void selfRR(struct Proceso *procesos, int num, int a, int b){
   int fin = 0;
   // ocupado -> procesador está libre
   int ocupado = 0;
+  int contadorQ = 1;
   int q = 1;
-  struct Proceso *aux;
-  int pAux;
-  int pMin;
-  int sAceptados, sNuevos;
-  int contadorQ;
-
-  printf("\n\n\n SRR con a=%d y b=%d:  ", a, b);
-
+  int prioridad=0;
+  int auxPrioridad=0;
+  int sNuevos;
+  int sAceptados;
+  int min=0;
   
-  // Si llegó un proceso en el tiempo 0 -> se agrega a la fila con prioridad 0.
-  if(procesos[contadorLlegada].llegada == tiempo){
-    procesos[contadorLlegada].ejecutado = 0;
-    colaAceptados.push(make_pair(0, &procesos[contadorLlegada]));
-    contadorLlegada++;
-  }
+  printf("\n\n\n SRR con q=1, a=%d y b=%d:  ", a, b);
+  
   
   while(!fin){
-
-    printf("\nTiempo: %d, colaAceptados: %d     colaNuevos: %d\n", tiempo, colaAceptados.size(), colaNuevos.size());
-    /*
-    if(cola.size()){
-      for(int k=0; k<cola.size(); k++){
-	aux = cola.front();
-	printf(" %c", aux->nombre);
-	cola.pop();
-	cola.push(aux);
+    
+    // Si ya llegó el proceso -> se agrega a la fila.
+    if(contadorLlegada<num && procesos[contadorLlegada].llegada == tiempo){
+      procesos[contadorLlegada].ejecutado = 0;
+      if(colaAceptados.size() == 0){
+	colaAceptados.push(make_pair(0, &procesos[contadorLlegada]));
+      }else{
+	colaNuevos.push(make_pair(0,&procesos[contadorLlegada]));
       }
-      printf("\n");
+      contadorLlegada++;
     }
-    */
-
+    
+    
     // Si no hay proceso
     if(!ocupado && colaAceptados.size()!=0){
       actual = colaAceptados.front().second;
       prioridad = colaAceptados.front().first;
+      colaAceptados.pop();
       ocupado = 1;
       if(actual->ejecutado == 0){
 	actual->inicio = tiempo;
       }
     }
-
+    
     // imprime nombre de trabajo
     if(ocupado){
       printf("%c", actual->nombre);
@@ -413,88 +456,76 @@ void selfRR(struct Proceso *procesos, int num, int a, int b){
     }
     tiempo++;
 
-    // aumentar y checar prioridad
+
+    // Aumenta prioridades y promueve procesos.
     sAceptados = colaAceptados.size();
-    sNuevos = colaNuevos.size();
-    pMin=100;
-    if(sAceptados==0){
-      pMin = 0;
+    if(sAceptados == 0){
+      if(actual!=NULL){
+	min = prioridad;
+      }else{
+	min = 0;
+      }
+    }else{
+      min = 100;
     }
     for(int i=0; i<sAceptados; i++){
       aux = colaAceptados.front().second;
-      pAux = colaAceptados.front().first;
-      pAux+=b;
+      auxPrioridad = colaAceptados.front().first+b;
+      if(auxPrioridad<min){
+	min = auxPrioridad;
+      }
       colaAceptados.pop();
-      if(pAux < pMin){
-	pMin = pAux;
-      }
-      colaAceptados.push(make_pair(pAux, aux));
+      colaAceptados.push(make_pair(auxPrioridad, aux));
+    }
+    sNuevos = colaNuevos.size();
+    for(int i=0; i<sNuevos; i++){
+      aux = colaNuevos.front().second;
+      auxPrioridad = colaNuevos.front().first+a;
+      colaNuevos.pop();
+      colaNuevos.push(make_pair(auxPrioridad, aux));
     }
     for(int i=0; i<sNuevos; i++){
       aux = colaNuevos.front().second;
-      pAux = colaNuevos.front().first;
-      pAux+=a;
+      auxPrioridad = colaNuevos.front().first;
       colaNuevos.pop();
-      colaNuevos.push(make_pair(pAux, aux));
-    }
-    for(int i=0; i<sNuevos; i++){
-      aux = colaNuevos.front().second;
-      pAux = colaNuevos.front().first;
-      colaNuevos.pop();
-      if(pAux>=pMin){
-	colaAceptados.push(make_pair(pAux, aux));
+      if(auxPrioridad>=min){
+	colaAceptados.push(make_pair(auxPrioridad, aux));
       }else{
-	colaNuevos.push(make_pair(pAux, aux));
+	colaNuevos.push(make_pair(auxPrioridad, aux));
       }
     }
-    
-    // Si ya llegó el proceso -> se agrega a la fila.
-    if(procesos[contadorLlegada].llegada == tiempo){
-      procesos[contadorLlegada].ejecutado = 0;
-      if(actual==NULL && colaAceptados.size()==0){
-	printf("\nLLEGUEEE %c a aceptados", procesos[contadorLlegada].nombre);
-	colaAceptados.push(make_pair(0, &procesos[contadorLlegada]));
-      }else{
-	printf("\nLLEGUEEE %c a nuevos", procesos[contadorLlegada].nombre);
-	colaNuevos.push(make_pair(0, &procesos[contadorLlegada]));
-      }
-      contadorLlegada++;
-    }
-    
+
     // Si ya acabó se calculan resultados individuales
-    if(actual->ejecutado == actual->requerido){
+    if(actual!=NULL && actual->ejecutado == actual->requerido){
       ocupado = 0;
       actual->termina = tiempo;
       actual->tiempo = actual->termina - actual->llegada;
       actual->espera = actual->tiempo - actual->requerido;
       actual->por = (float)actual->tiempo/actual->requerido;
-      colaAceptados.pop();
       actual=NULL;
       contadorQ=0;
       
-      // checa si ya llegaron todos y ya no hay en la cola
+      // checa si es llegaron todos y ya no hay en la cola
       if(contadorLlegada == num && colaAceptados.size()==0){
 	fin=1;
       }
-    }else if(contadorQ==q){
-      ocupado = 0;
-      aux = colaAceptados.front().second;
-      pAux = colaAceptados.front().first;
-      colaAceptados.pop();
-      colaAceptados.push(make_pair(pAux, aux));
       
+    }else if(contadorQ==q && ocupado){
+      ocupado = 0;
+      colaAceptados.push(make_pair(prioridad, actual));
       contadorQ = 0;
     }
+    
     contadorQ++;
     
   }
   
   printf("\n");
-
   
 }
 
 
+// Se imprime en pantalla los datos iniciales de los procesos a simular
 void imprimirCarga(struct Proceso *procesos, int num){
 
   float promedio = 0;
@@ -516,6 +547,7 @@ void imprimirCarga(struct Proceso *procesos, int num){
   
 }
 
+// Se imprime en pantalla los resultados de los datos a simular.
 void imprimirResultados(struct Proceso *procesos, int num, float *prome, int idxProme){
 
   float promedioT = 0;
@@ -542,6 +574,8 @@ void imprimirResultados(struct Proceso *procesos, int num, float *prome, int idx
 }
 
 
+// Se simulan los diferentes algoritmos con una carga específica.
+// y se muestran resultados.
 void simulacion(struct Proceso *procesos, int num){
 
   float prome[21];
@@ -555,10 +589,10 @@ void simulacion(struct Proceso *procesos, int num){
   imprimirResultados(procesos, num, prome, 6);
   shortNext(procesos, num);
   imprimirResultados(procesos, num, prome, 9);
-  fb(procesos, num, 1, 0);
-  imprimirResultados(procesos, num, prome, 12);
-  fb(procesos, num, 1, 1);
-  imprimirResultados(procesos, num, prome, 15);
+  //fb(procesos, num, 1, 0);
+  //imprimirResultados(procesos, num, prome, 12);
+  //fb(procesos, num, 1, 1);
+  //imprimirResultados(procesos, num, prome, 15);
   selfRR(procesos, num, 2, 1);
   imprimirResultados(procesos, num, prome, 18);
 
@@ -567,8 +601,8 @@ void simulacion(struct Proceso *procesos, int num){
   printf(" RR1 :\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[3], prome[4], prome[5]);
   printf(" RR4 :\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[6], prome[7], prome[8]);
   printf(" SPN :\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[9], prome[10], prome[11]);
-  printf(" FB1 :\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[12], prome[13], prome[14]);
-  printf(" FB2^n:\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[15], prome[16], prome[17]);
+  //printf(" FB1 :\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[12], prome[13], prome[14]);
+  //printf(" FB2^n:\t\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[15], prome[16], prome[17]);
   printf(" SRR-21:\tT: %2.2f\tE: %2.2f\tP: %2.2f\n", prome[18], prome[19], prome[20]);
   
 }
@@ -584,7 +618,8 @@ int main() {
   printf("\n\n ~ ~ ~ Bienvenido al simulador de algoritmos de planificacion. ~ ~ ~\n\n");
   printf(" Se presentan 4 simulaciones para los algoritmos, la vista en\n");
   printf(" clase y otras 3 aleatorias de 5, 10 y 15 procesos respectivamente.\n\n");
-  
+
+  //Ejemplo en clase -- inicialización
   ejemplo[0].nombre = 'A';
   ejemplo[0].llegada = 0;
   ejemplo[0].requerido = 3;
@@ -605,9 +640,8 @@ int main() {
   printf("\n Tabla de procesos, caso 1:\n");
 
   simulacion(ejemplo, 5);
-  /*
 
-
+  // Aleatorio de 5 procesos.
   printf("\n\n\n-----------------------------------------------------------------------------");
   printf("\n Tabla de procesos, caso 2:\n");
   aux = 0;
@@ -624,6 +658,7 @@ int main() {
   simulacion(lista1, 5);
 
 
+  // Aleatorio de 10 procesos.
   printf("\n\n\n-----------------------------------------------------------------------------");
   printf("\n Tabla de procesos, caso 3:\n");
   aux = 0;
@@ -639,7 +674,7 @@ int main() {
 
   simulacion(lista2, 10);
 
-
+  // Aleatorio de 15 procesos.
   printf("\n\n\n-----------------------------------------------------------------------------");
   printf("\n Tabla de procesos, caso 4:\n");
   aux = 0;
@@ -655,7 +690,6 @@ int main() {
 
   simulacion(lista3, 15);
 
-   */
 
   printf("\n\n\n\n\n - - - - - - - - - - - - - -  FINAL SIMULACIÓN - - - - - - - - - - - - - \n\n\n\n");
   
