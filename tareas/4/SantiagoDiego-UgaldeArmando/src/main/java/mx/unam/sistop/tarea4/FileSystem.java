5d8e6840543830703f6eedd01515891c504f540a
Armando Ugalde <armauv@gmail.com>
2021-11-30 10:04:35 -0600

    Implementa comandos dir y quit

--- tareas/4/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea4/FileSystem.java
+++ tareas/4/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea4/FileSystem.java
@@ -1,6 +1,7 @@
 package mx.unam.sistop.tarea4;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class FileSystem {
@@ -8,7 +9,50 @@ public class FileSystem {
 
     public static void main(String[] args) {
         initialize();
-        handleDir();
+        printUsage();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().strip();
+            String[] split = line.split(" ");
+
+            if (split.length == 0 || split.length > 3) {
+                printUsage();
+                continue;
+            }
+
+            switch (split[0]) {
+                case "dir":
+                    if (split.length == 1) handleDir();
+                    else printUsage();
+                    break;
+
+                case "quit":
+                    if (split.length == 1) return;
+                    else printUsage();
+                    break;
+
+                default:
+                    printUsage();
+            }
+        }
+
+
+    }
+
+    private static void printUsage() {
+        System.out.println("\nUso:");
+        System.out.println("dir → Muestra el directorio");
+        System.out.println("open <arch> <modo> → Especifica que operaremos con el archivo de nombre \"arch\", " +
+                "empleando el modo especificado. Entrega un descriptor de archivo numérico.");
+        System.out.println("close <descr> → Termina una sesión de trabajo con el archivo referido por el descriptor " +
+                "indicado. Después de un close, cualquier intento por usar ese archivo entregará error.");
+        System.out.println("read <descr> <longitud> → Lee la cantidad de bytes especificada");
+        System.out.println("write <descr> <longitud> <datos» → Escribe la cantidad de bytes especificada, guardando " +
+                "los datos indicados como parámetro.");
+        System.out.println("seek <descr> <ubicacion> → Salta a la ubicación especificada del archivo");
+        System.out.println("quit → Detiene la ejecucin de la simulación\n");
     }
 
     private static void initialize() {
