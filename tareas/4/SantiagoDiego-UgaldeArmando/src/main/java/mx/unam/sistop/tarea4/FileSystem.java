76716ea39fee2e25c0d5e18b2d5255809f670de6
Armando Ugalde <armauv@gmail.com>
2021-11-29 23:00:31 -0600

    Implementa inicialización y manejo de dir

--- tareas/4/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea4/FileSystem.java
+++ tareas/4/SantiagoDiego-UgaldeArmando/src/main/java/mx/unam/sistop/tarea4/FileSystem.java
@@ -1,7 +1,27 @@
 package mx.unam.sistop.tarea4;
 
-public class FileSystem {
-    public static void main(String[] args) {
+import java.util.HashSet;
+import java.util.Set;
 
+public class FileSystem {
+    private static final Set<SimulatedFile> files = new HashSet<>();
+
+    public static void main(String[] args) {
+        initialize();
+        handleDir();
+    }
+
+    private static void initialize() {
+        files.add(new SimulatedFile("arch1", "012345678901234567890123456789012345678901234567890123456789"));
+        files.add(new SimulatedFile("arch2", "432903249803432754832574897589437587349857349875983471"));
+        files.add(new SimulatedFile("helloworld", "320948325093428932617647812661464876fsdafasddasfdfsdasdf"));
+        files.add(new SimulatedFile("stgut.txt", "dasfdasofjnasdofjfdjoasioasfdafsd"));
+    }
+
+    private static void handleDir() {
+        for (SimulatedFile file : files) {
+            System.out.print(file.getFilename() + " [" + file.getSize() + " bytes]\t");
+        }
+        System.out.println();
     }
 }
